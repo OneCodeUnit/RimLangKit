c28bfe501e57dee728e1dcba57010006ff791e84
Maksim <inquisitor914@gmail.com>
2023-07-23 12:47:50 +0300

    обновление версии

--- CommentInsert.cs
+++ CommentInsert.cs
@@ -52,7 +52,7 @@ namespace RimLangKit
     }
 
     //Класс, в котором описывается не до конца понятный мне трюк, позволяющий вписывать специальные символы «как есть»
-    internal class XRaw : XText
+    internal sealed class XRaw : XText
     {
         public XRaw(string text) : base(text) { }
         public XRaw(XText text) : base(text) { }
